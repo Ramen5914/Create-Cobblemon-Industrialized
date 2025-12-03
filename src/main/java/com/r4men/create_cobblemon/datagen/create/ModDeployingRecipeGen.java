52f7e048c07b0e44415d2382d5de03c24d73ab1a
Omar Rahman <omar@r4men.com>
2025-12-03 05:34:56 -0800

    Optimized imports

--- src/main/java/com/r4men/create_cobblemon/datagen/create/ModDeployingRecipeGen.java
+++ src/main/java/com/r4men/create_cobblemon/datagen/create/ModDeployingRecipeGen.java
@@ -7,7 +7,6 @@ import com.simibubi.create.api.data.recipe.DeployingRecipeGen;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.item.Items;
-import net.neoforged.neoforge.common.Tags;
 
 import java.util.concurrent.CompletableFuture;
 
